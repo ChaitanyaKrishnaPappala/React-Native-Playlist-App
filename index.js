7ff0f4ed7f4f81222d47a96b5d3e1835eb095738
ChaitanyaKrishnaPappala <cpappala@chaitanyas-mbp.attlocal.net>
2018-07-21 17:22:42 -0500

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
-const App = () => (<Header />);
+const App = () => (<Header headerText={'Albums!'} />);
 
 // used to render on device
 AppRegistry.registerComponent('albums', () => App);
--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 
-const Header = () => {
+const Header = (props) => {
     const { textStyle, viewStyle } = styles;
 
     return (
     <View style={viewStyle}>
-        <Text style={textStyle}>Albums!</Text>
+        <Text style={textStyle}>{props.headerText}</Text>
     </View>);
 };
 
 
 const styles = {
     textStyle: {
-        color: '#F8B29C',
         fontSize: 20
     },
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        padding: 15,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25
     }
 };
 
